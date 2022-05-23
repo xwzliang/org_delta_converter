597e5b53f5e9813346f0b916f834ed861f456ed9
broliang <18706703437@163.com>
2022-05-23 16:25:58 +0800

    Implement some conversion from org to delta.

--- lib/org_delta_converter.dart
+++ lib/org_delta_converter.dart
@@ -1,7 +1,3 @@
 library org_delta_converter;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/org_to_delta.dart';
---
+++ lib/src/org_to_delta.dart
@@ -0,0 +1,48 @@
+import 'dart:convert';
+
+import 'package:flutter_quill/flutter_quill.dart';
+import 'package:org_parser/org_parser.dart';
+
+/// Convert from org object parsed by package org_parser to delta
+class OrgToDelta {}
+
+/// Convert from org object parsed by package org_parser to delta
+class OrgNodesToDeltaConverter {
+  final _delta = Delta();
+  Delta convert(List<OrgNode>? orgNodes) {
+    if (orgNodes != null) {
+      orgNodeListToDelta(orgNodes);
+    }
+    return _delta;
+  }
+
+  void orgNodeListToDelta(List<OrgNode> orgNodes) {
+    for (final orgNode in orgNodes) {
+      orgNodeToDelta(orgNode);
+    }
+  }
+
+  void orgNodeToDelta(OrgNode orgNode) {
+    if (orgNode is OrgPlainText) {
+      _delta.insert(orgNode.content);
+    } else if (orgNode is OrgLink) {
+      _delta.insert(orgNode.description, {
+        Attribute.link.key: orgNode.location,
+      });
+    } else if (orgNode is OrgSection) {
+      _delta.insert(orgNode.headline.rawTitle);
+      _delta.insert('\n', {
+        Attribute.header.key: orgNode.headline.level,
+        // Attribute.indent.key: orgNode.headline.level - 1,
+      });
+      if (orgNode.content != null) {
+        orgNodeToDelta(orgNode.content!);
+      }
+      orgNodeListToDelta(orgNode.sections);
+    } else if (orgNode is OrgContent) {
+      orgNodeListToDelta(orgNode.children);
+    } else if (orgNode is OrgParagraph) {
+      orgNodeListToDelta(orgNode.body.children);
+    }
+  }
+}
